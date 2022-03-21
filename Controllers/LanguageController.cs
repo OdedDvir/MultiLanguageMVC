8d93368c6965bfdba97c9fcf00a8116861002d88
Oded Dvir Nachum <odeddvirn@gmail.com>
2022-03-22 01:56:16 +0200

    Add project files.

---
+++ Controllers/LanguageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanguage.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(string language)
+        {
+            if (language != null)
+            {
+                Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie("Langauge");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View("Index");
+        }
+    }
+}
